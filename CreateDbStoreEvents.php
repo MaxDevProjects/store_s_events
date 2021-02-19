70ee3abc537bb24fd04f3579c7dbbfdbdefc399f
MaxDevProjects <proad.maxime.martin@gmail.com>
2021-02-19 01:19:10 +0100

    creation de la base de donnée

---
+++ CreateDbStoreEvents.php
@@ -0,0 +1,63 @@
+<?php
+global $version_db;
+$version_db = '1.0';
+/**
+ * Method store_events_install
+ *
+ * @return void
+ */
+function store_events_install()
+{
+    global $wpdb;
+    global $version_db;
+    $installed_ver = get_option('version_db');
+    if ($installed_ver != $version_db) {
+        $table_name = $wpdb->prefix . 'store_events';
+
+        $sql = "CREATE TABLE $table_name (
+            id MEDIUMINT(9) NOT NULL AUTO_INCREMENT,
+            date_depart DATE NOT NULL,
+            date_retour DATE,
+            message TEXT(255) DEFAULT '',
+            PRIMARY KEY (id)
+        )";
+
+        include_once ABSPATH . 'wp-admin/includes/upgrade.php';
+        dbDelta($sql);
+        add_option('version_db', $version_db);
+    }
+}
+
+/**
+ * Method store_events_install_data
+ *
+ * @return void
+ */
+function store_events_install_data()
+{
+    global $wpdb;
+    $welcome_text = 'Congratulations, you just completed the installation!';
+
+    $table_name = $wpdb->prefix . 'store_events';
+
+    $wpdb->insert(
+        $table_name,
+        array(
+            'date_depart' => '2021-02-15',
+            'date_retour' => '2021-02-25',
+            'message' => $welcome_text,
+        )
+    );
+}
+/**
+ * Method myplugin_update_db_check
+ *
+ * @return void
+ */
+function myplugin_update_db_check()
+{
+    global $version_db;
+    if (get_site_option('version_db') != $version_db) {
+        store_events_install();
+    }
+}
--- StoresEvents.php
+++ StoresEvents.php
@@ -1,92 +1,117 @@
 <?php
 /*
- * Plugin Name: Store's events
+Plugin Name: Store events
 Plugin URI: null
-Description: This pluggin is for alert customer of your site. Hey its the Holidays 🌴 you must be wait my come back for to receive your command
+Description: Notify customers about events store.
 Author: Maxime G. Martin ( Clower Web Conception )
-Version: 0.0.1
+Version: 1.0.0
 Author URI: https://www.clower.fr/
  */
-
+/**
+ * StoresEvents
+ */
 class StoresEvents
 {
+    /**
+     * Method __construct
+     *
+     * @return void
+     */
     public function __construct()
     {
+        if (!defined('ABSPATH')) {
+            define('ABSPATH', dirname(__FILE__) . '/');
+        }
         $this->hooks();
     }
 
-    function func_load_script() {
-        wp_enqueue_script( 'my-vue-app-vendors',
-            plugins_url( '/build/js/chunk-vendors.js', __FILE__ ),
+    /**
+     * Method func_load_script
+     *
+     * @return void
+     */
+    public function func_load_script()
+    {
+        wp_enqueue_script('my-vue-app-vendors',
+            plugins_url('/build/js/chunk-vendors.js', __FILE__),
             array(),
             '1.0.0'
         );
-        wp_enqueue_script( 'my-vue-app',
-            plugins_url( '/build/js/app.js', __FILE__ ),
-            array( 'my-vue-app-vendors' ),
+        wp_enqueue_script('my-vue-app',
+            plugins_url('/build/js/app.js', __FILE__),
+            array('my-vue-app-vendors'),
             '1.0.0'
         );
-        wp_enqueue_style( 'my-vue-app',
-            plugins_url( '/build/css/app.css', __FILE__),
+        wp_enqueue_style('my-vue-app',
+            plugins_url('/build/css/app.css', __FILE__),
             array(),
             '1.0.0'
         );
     }
 
-    /*
-     * Informations should be return for display the plugin in admin menu
-     */
-    public function admin_store_s_events_plugin_menu() {
+/*
+ * Informations should be return for display the plugin in admin menu
+ */
+    public function admin_store_s_events_plugin_menu()
+    {
         add_menu_page(
-            __('Store\'s events', 'store-s-events'),    //titre de la page
-            __('Store\'s events', 'store-s-events'),    // titre du menu
+            __('Store\'s events', 'store-s-events'), //titre de la page
+            __('Store\'s events', 'store-s-events'), // titre du menu
             'manage_options', //Capability
             'store-s-events-plugin', //Slug
-            [&$this, 'load_store_s_events_plugin_page'] // Callback page function
+            [ & $this, 'load_store_s_events_plugin_page']// Callback page function
         );
     }
 
-    // content of plugin page
-    public function load_store_s_events_plugin_page() {
-        echo '<h1>'. __('Store\'s events', 'store-s-events') . '</h1>';
-        echo '<p>'. __('Notify your customers about <strong>Store\'s events</strong> 🌴', 'store-s-events') . '</p>';
+// content of plugin page
+    public function load_store_s_events_plugin_page()
+    {
+        echo '<h1>' . __('Store\'s events', 'store-s-events') . '</h1>';
+        echo '<p>' . __('Notify your customers about <strong>Store\'s events</strong> 🌴', 'store-s-events') . '</p>';
         $this->store_s_events_content();
     }
 
-    function store_s_events_content() {
+/**
+ * Method store_s_events_content
+ *
+ * @return void
+ */
+    public function store_s_events_content()
+    {
         $this->func_load_script();
         echo "<div id='app'></div>";
     }
 
     private function hooks()
     {
+        add_action('plugins_loaded', 'myplugin_update_db_check');
         //add plugin in admin menu list
         add_action("admin_menu", [$this, 'admin_store_s_events_plugin_menu']);
-//        //add action for translate plugin
-//        add_action('plugins_loaded', [$this, 'load_plugin_text_domain']);
+        //        //add action for translate plugin
+        //        add_action('plugins_loaded', [$this, 'load_plugin_text_domain']);
         //hook de chargement des scripts
-        add_action('wp_enqueue_scripts',[$this, 'func_load_script']);
-        add_shortcode( 'wpvue', 'store_s_events_content' );
-
+        add_action('wp_enqueue_scripts', [$this, 'func_load_script']);
+        add_shortcode('wpvue', 'store_s_events_content');
 
         /**
          *Script that import modules must use a script tag with type="module",
          * so let's set it for the script.
          */
-        add_filter( 'script_loader_tag', function ( $tag, $handle, $src ) {
+        add_filter('script_loader_tag', function ($tag, $handle, $src) {
 
-            switch ( $handle ) {
+            switch ($handle) {
                 case 'main':
-                    return '<script type="module" src="' . esc_url( $src ) . '"></script>';
+                    return '<script type="module" src="' . esc_url($src) . '"></script>';
                     break;
 
                 default:
                     return $tag;
                     break;
             }
-
-        }, 10, 3 );
+        }, 10, 3);
     }
-}
-
+};
+include_once 'CreateDbStoreEvents.php';
+register_activation_hook(__FILE__, 'store_events_install');
+register_activation_hook(__FILE__, 'store_events_install_data');
 new StoresEvents();
